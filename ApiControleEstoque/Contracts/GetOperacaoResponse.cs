c0cae1a2ec0b461107fdd3e2672ae7df7187e29b
felipepereirafdpp <felipepereira.fdpp@gmail.com>
2026-06-17 16:09:49 -0300

    Subindo DTO

--- ApiControleEstoque/Contracts/GetOperacaoResponse.cs
+++ ApiControleEstoque/Contracts/GetOperacaoResponse.cs
@@ -1,10 +1,24 @@
-﻿namespace ApiControleEstoque.Contracts
+﻿using OperacaoDomain;
+using ProdutoDomain;
+
+namespace ApiControleEstoque.Contracts
 {
     public class GetOperacaoResponse
     {
         public Guid Id { get; set; }
         public DateTime Hora { get; set; }
-
         public required string Motivo { get; set;}
+
+        public List<OperacaoEstoqueDetalheDTO> Detalhes { get; set; } = [];
+
     }
+        public class OperacaoEstoqueDetalheDTO {
+
+            public Guid Id { get; set; }
+            public decimal Quantidade { get; set; }
+            public string NomeProduto { get; set; }
+            public string Sigla { get; set; }
+
+
+        }
 }
